c1c32a9ac1f89e23db12e2606318855470b22ad1
Kyle Cleary <kscleary11@gmail.com>
2024-07-06 15:22:56 -0600

    Exercise Complete

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(2, 2));
+            Console.WriteLine(Add(3.12M, 4.23M));
+            Console.WriteLine(Add(1, 0, false));
+            Console.WriteLine(Add(2, 5, true));
+
+               
+        }
+
+        public static int Add(int x, int y)
+        {
+            return (x + y);
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return (x + y);
+
+        }
+
+        public static string Add(int x, int y, bool a)
+        {
+            
+            if (a && (x + y) > 1)
+            {
+                return $"You have {x + y} dollars.";
+
+            }
+            else return $"You have {x + y} dollar.";
         }
     }
 }
